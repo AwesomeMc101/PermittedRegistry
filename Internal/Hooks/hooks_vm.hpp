ab667045c01912d94e5b81b4c92e120ec363c4e6
AwesomeMc101 <43798963+AwesomeMc101@users.noreply.github.com>
2025-06-10 14:30:54 -0500

    Create hooks_vm.hpp

---
+++ Internal/Hooks/hooks_vm.hpp
@@ -0,0 +1,13 @@
+/*
+hooks_vm.hpp
+-> Set hooks for registry value modification functions.
+-> By AwesomeMc101/CDH for Tensora Softworks
+6/10/25
+*/
+
+#include <Windows.h>
+#include <string>
+#include <ntstatus.h>
+
+BOOL hookZwSetValueKey();
+BOOL hookZwDeleteValueKey();
